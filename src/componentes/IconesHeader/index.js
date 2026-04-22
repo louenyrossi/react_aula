977a9b178c830cf707258f06bceff2c87ba245d7
Loueny <loueny.rossi@aluno.senai.br>
2026-04-22 12:05:29 -0300

    isso ai

---
+++ src/componentes/IconesHeader/index.js
@@ -0,0 +1,9 @@
+function IconesHeader() {
+    return (
+        <ul className='icones'>
+            {icones.map((icone) => (
+                <li><img src={icone}></img></li>
+            ))}
+        </ul>
+    )
+}
